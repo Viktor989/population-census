982501cb99dcdc5f2167b2f233994f902b989597
Viktor989 <golikov-vn@mail.ru>
2022-04-21 23:44:42 +0400

    замена двух стримов конструкцией ИЛИ

--- src/ViktorGolikov/Main.java
+++ src/ViktorGolikov/Main.java
@@ -1,17 +1,14 @@
 package ViktorGolikov;
 
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
-
-
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Петр", "Виктор", "Ирина", "Егор", "Игорь", "Юлия");
         List<String> families = Arrays.asList("Иванов", "Петров", "Сидоров", "Петросян", "Пугачева", "Кабзон", "Львов");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
@@ -20,7 +17,6 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
-
         long count = persons.stream()
                 .filter(х -> х.getAge() < 18)
                 .count();
@@ -36,20 +32,15 @@ public class Main {
         System.out.println(conscripts);
 
         System.out.println("Список потенциально работоспособного населения");
-        List<Person> workableMen = persons.stream()
+        List<Person> workable = persons.stream()
                 .filter(x -> x.getAge() >= 18 )
-                .filter(y -> y.getAge() <= 60 && y.getSex().equals(Sex.MAN))
+                .filter(y -> y.getAge() <= 60 && y.getSex().equals(Sex.MAN) || y.getAge() <= 55 && y.getSex().equals(Sex.WOMAN))
                 .filter(z -> z.getEducation().equals(Education.HIGHER))
                 .sorted(Comparator.comparing(Person::getFamily))
                 .collect(Collectors.toList());
-        System.out.println(workableMen);
-        List<Person> workableWoman = persons.stream()
-                .filter(x -> x.getAge() >= 18 )
-                .filter(i -> i.getAge() <= 55 && i.getSex().equals(Sex.WOMAN))
-                .filter(z -> z.getEducation().equals(Education.HIGHER))
-                .sorted(Comparator.comparing(Person::getFamily))
-                .collect(Collectors.toList());
-        System.out.println(workableWoman);
+        System.out.println(workable);
+
+
 
     }
 }
